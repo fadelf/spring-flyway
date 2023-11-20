c11f342b93bfbd6b6a6f3692f3a312dbe7f5ff45
FadhelF <fadhelfaturrahman@gmail.com>
2023-11-20 22:47:59 +0700

    Initial project

---
+++ src/test/java/com/delfa/springflyway/SpringFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfa.springflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
